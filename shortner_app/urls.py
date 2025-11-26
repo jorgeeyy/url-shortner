8bc91f8ba5754f6e825deb459d1401cf8df5c0e2
jorgeeyy <jqubez@gmail.com>
2025-11-26 15:04:29 +0000

    made some changes and added secrets to env

--- shortner_app/urls.py
+++ shortner_app/urls.py
@@ -4,5 +4,5 @@ from .views import ShortenURLView, RedirectView, URLHistoryView
 urlpatterns = [
     path("api/shorten/", ShortenURLView.as_view(), name="shorten-url"),
     path("api/history/", URLHistoryView.as_view(), name="url-history"),
-    path("api/<str:code>/", RedirectView.as_view(), name="redirect-url"),
+    path("<str:code>/", RedirectView.as_view(), name="redirect-url"),
 ]
